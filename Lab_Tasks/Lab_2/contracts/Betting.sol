5b4af6518aa2079b94d6d7f44f9764b9bbf63c02
Ravi Pandhi <rpandhi2018@my.fit.edu>
2019-11-26 20:22:49 -0500

    betting contract complete

--- Lab_Tasks/Lab_2/contracts/Betting.sol
+++ Lab_Tasks/Lab_2/contracts/Betting.sol
@@ -3,14 +3,20 @@ pragma solidity ^0.5.11;
 //import "../contracts/strings.sol";
 
 contract Betting {
-    address contract_owner;
+
+
+
+    //contract owner and oracle can only be set once at the beginning.
+    address contract_owner;  //there can only be one contract owner, so no 'set contract' method specified.
     address event_oracle;
+
+
     address[] public gamblers;
     address[] public list_of_winners;
+    uint[] all_the_possible_outcomes;
+    uint[] winn_amnt;
 
-    uint outcome_1;
-    uint outcome_2;
-    uint outcome_3;
+    bool auction_closed = false;
 
     mapping(address => uint) private map_addr_bet_amnt;
     mapping(address => uint) private map_addr_bet_outc;
@@ -21,56 +27,89 @@ contract Betting {
     {
         //the contract owner is set.
         contract_owner = msg.sender;
+        emit owner_oracle_set_broadcast(msg.sender, "Owner is set now", address(this));
     }
+
+    // all the events --start
+
+    event owner_oracle_set_broadcast(
+        address indexed _from,
+        string _value,
+        address contract_address
+    );
+
+    event bet(
+        address indexed _from,
+        uint _value,
+        string msg,
+        uint bet_value
+    );
+
+    event possible_outcomes_broadcast(
+        address indexed _from,
+        uint[] _value,
+        string msg
+    );
+
+    event common_event_msg(
+        string common_msg
+    );
+
+    // all the events --end
 
     function set_oracle(address input) public{
+
+        require(event_oracle==address(0), "Oracle cannot be reset");
         event_oracle = input;
+        emit owner_oracle_set_broadcast(event_oracle,"Oracle is set now",address(this));
     }
 
-    function get_oracle() public view returns (address){
-        return event_oracle;
-    }
+    // function get_oracle() public view returns (address){
+    //     return event_oracle;
+    // }
 
-    function get_owner() public view returns (address){
-        return contract_owner;
-    }
+    // function get_owner() public view returns (address){
+    //     return contract_owner;
+    // }
 
+    function get_win_amnt() public view returns (uint[] memory){
+        return winn_amnt;
+    }
     function get_gamblers_list() public view returns (address[] memory){
         return gamblers;
     }
 
-    function set_outcomes(uint inp1, uint inp2, uint inp3) public{
-        outcome_1 = inp1;
-        outcome_2 = inp2;
-        outcome_3 = inp3;
+    function set_outcomes(uint[] memory outcomes) public{
+        require(msg.sender==contract_owner, "Only the contract owner can set the outcomes");
+        all_the_possible_outcomes = outcomes;
+        emit possible_outcomes_broadcast(msg.sender, all_the_possible_outcomes, "The outcomes have been set");
     }
 
-    // event bet(
-    //     address indexed _from,
-    //     uint _value,
-    //     uint bet_value
-    // );
 
-    function make_a_bet(uint expected_outcome, uint bet_amount) public payable returns (uint,uint,address[] memory)
+    function make_a_bet(uint expected_outcome) public payable returns (uint,uint,address[] memory)
     {
         //validation starts
+        //the oracle should be set inorder to avoid oracle gambling first and then becoming oracle afterwards
+        require(event_oracle!=address(0), "Oracle must be set before you can make a bet");
         //should not be an owner
         require(msg.sender!=contract_owner, "Owner cannot gamble");
         //should not be an oracle
         require(msg.sender!=event_oracle, "Oracle cannot gamble");
         //should not have placed a bet before
         require(map_addr_bet_amnt[msg.sender]==0, "You have already plavced a bet!");
+        //should not place an empty bet (0)
+        require(msg.value>0,"Please specify an amount");
         //validation ends
 
         //make a bet
-        map_addr_bet_amnt[msg.sender] = bet_amount;
+        map_addr_bet_amnt[msg.sender] = msg.value;
         map_addr_bet_outc[msg.sender] = expected_outcome;
 
         //update variables
         gamblers.push(msg.sender);
 
         //emit an event to the blockchain
-        // emit bet(msg.sender, amount, expected_outcome);
+        emit bet(msg.sender, msg.value, "A bet was just made", expected_outcome);
 
         return (map_addr_bet_amnt[msg.sender],map_addr_bet_outc[msg.sender],gamblers);
 
@@ -78,14 +117,14 @@ contract Betting {
 
     function decide_winners(uint correct_outcome) public{
 
-        uint j = 0;
+        
 
-        //calculate if we have a same bet and also, decide the list of winners.
+        //whom should the betting amount go to.
         for (uint i = 0; i<gamblers.length; i++)
         {
             if(map_addr_bet_outc[gamblers[i]]==correct_outcome)
             {
-                list_of_winners[j++] = gamblers[i];
+                list_of_winners.push(gamblers[i]);
             }
         }
 
@@ -96,8 +135,10 @@ contract Betting {
         return list_of_winners;
     }
 
-    function same_bet() public view returns(bool){
+    function is_the_bet_same() public view returns(bool){
         bool same_bet_bool = true;
+
+        //comparing if everyone has the same bet
         for (uint i = 0; i<gamblers.length; i++)
         {
             if(i!=gamblers.length-1 && map_addr_bet_outc[gamblers[i]] != map_addr_bet_outc[gamblers[i+1]])
@@ -105,6 +146,7 @@ contract Betting {
                 same_bet_bool = false;
             }
         }
+
         return same_bet_bool;
     }
 
@@ -112,16 +154,20 @@ contract Betting {
         //validation starts
 
         //validate that all the gamblers have bet
+        require(gamblers.length==2,"All the gamblers (2) have not placed a bet yet.");
 
         //validate that you are the oracle
         require(msg.sender==event_oracle, "Only the oracle can decide the outcome");
 
+        //validate if the betting is still open
+        require(auction_closed==false, "Betting is closed now");
+    
         //validation ends
 
         //check if everyone has the same bet
 
-        bool same_bet_bool = same_bet();
-        decide_winners(correct_outcome);
+        bool same_bet_bool = is_the_bet_same();
+        
 
         if(same_bet_bool)
         {
@@ -130,14 +176,19 @@ contract Betting {
             {
                 map_addr_bet_win_amt[gamblers[i]] = map_addr_bet_amnt[gamblers[i]];
             }
+            emit common_event_msg("all bet on the same outcome, take your bet money back");
             //emit event : everyone bet on the same thing, not possible. take your money back
         }
         else
         {
+            //this function decides who are the winners as per the outcome defined by the oracle.
+            //decide_winners(correct_outcome);
+
             uint total_bet_amount = get_total_bet_amount();
             if(list_of_winners.length==0)
             {
                 //emit event: no one had good luck. all the money goes to the oracle who decided the outcome
+                emit common_event_msg("No one wins but the oracle.");
                 map_addr_bet_win_amt[event_oracle] = total_bet_amount;
 
             }
@@ -145,10 +196,14 @@ contract Betting {
             {
                 //emit event: the winners have been decided, you can request to withdraw to see if you have won.
                 //give the amount on the basis of fair division.
+                emit common_event_msg("The winners have been decided. You can try and withdraw to see if you have won.");
                 uint num_of_winners = list_of_winners.length;
+                uint amnt;
                 for(uint i = 0;i<num_of_winners;i++)
                 {
-                    map_addr_bet_win_amt[list_of_winners[i]] = total_bet_amount/num_of_winners; //need to make changes for uneven division.
+                    amnt = uint(total_bet_amount) / uint(num_of_winners);
+                    winn_amnt.push(amnt);
+                    map_addr_bet_win_amt[list_of_winners[i]] = amnt; //need to make changes for uneven division.
                 }
             }
         }
@@ -166,6 +221,8 @@ contract Betting {
 
         return total_amount;
     }
+
+
     /**
      This function allows the winners to withdraw their winning amount from the contract.
      */
@@ -173,16 +230,15 @@ contract Betting {
         //validation starts
         //withdrawer should be a winner
         require(map_addr_bet_win_amt[msg.sender]!=0, "You are not the winner, sorry!");
+        require(map_addr_bet_win_amt[msg.sender] >= amount, "You have withdrawn your amount");
 
-        if(map_addr_bet_win_amt[msg.sender] >= amount)
+        map_addr_bet_win_amt[msg.sender] -= amount;
+
+        if(!msg.sender.send(amount))
         {
-            map_addr_bet_win_amt[msg.sender] -= amount;
-
-            if(!msg.sender.send(amount))
-            {
-                map_addr_bet_win_amt[msg.sender] -= amount;
-            }
+            map_addr_bet_win_amt[msg.sender] += amount;
         }
+
         return map_addr_bet_win_amt[msg.sender];
     }
 }
